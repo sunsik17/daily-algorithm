c4edf6dcb32fcd28b9ff14a238d22201aace2895
sunsik17 <17sunsik@gmail.com>
2025-01-19 01:41:59 +0900

    출력문 제거

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -5,7 +5,6 @@ class Solution {
         int answer = 1;
 		Arrays.sort(routes, (x, y) -> x[1] == y[1] ? x[0] - y[0] : x[1] - y[1]);
         int pivot = routes[0][1];
-        System.out.print(Arrays.deepToString(routes));
         for (int i = 1; i < routes.length; i++) {
             if (routes[i][0] <= pivot) continue;
             pivot = routes[i][1];
